7d36b0a1c447d04274fe96cd0be5a4c71b2889bc
Paul Weber <work@paul-weber.de>
2022-02-16 18:06:37 +0100

    Add user for apple review (#3)

--- src/main/kotlin/de/paulweber/Database.kt
+++ src/main/kotlin/de/paulweber/Database.kt
@@ -6,7 +6,7 @@ import de.paulweber.routes.profile.Profile
 import de.paulweber.routes.transaction.Transaction
 import de.paulweber.routes.user.User
 
-val userStore = mutableSetOf<User>()
+val userStore = mutableSetOf<User>(User.Registered.factoryCreate("apple@example.com", "aasdzuhjbnmhuawezajsd65"))
 val refreshTokenStore = mutableMapOf<String, MutableSet<String>>()
 val receiverCodeStore = mutableMapOf<String, String>()
 val profileStore = mutableMapOf<String, Profile>()
